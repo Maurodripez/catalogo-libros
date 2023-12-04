debea041cd274e4bae335f112ac57d06e4243a7e
Mauricio Rodríguez <maurodripez@gmail.com>
2023-12-04 12:08:34 -0600

    Initial commit

---
+++ src/main/java/com/mauamott/catalogolibros/CatalogoLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.mauamott.catalogolibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauamott/catalogolibros/CatalogoLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauamott.catalogolibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
